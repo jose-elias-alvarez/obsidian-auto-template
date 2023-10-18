7649dab2243553e4dbd15f9b93c45a42577939f0
Jose Alvarez <jealv@icloud.com>
2023-10-18 15:51:44 -0400

    test: add more cases

--- src/utils/parse-patterns.test.ts
+++ src/utils/parse-patterns.test.ts
@@ -1,20 +1,27 @@
 import parsePatterns from "./parse-patterns";
 
 describe("parsePatterns", () => {
-	it("returns an empty array when given an empty string", () => {
+	it("returns an empty array when given an empty pattern", () => {
 		const patterns = "";
 		const result = parsePatterns(patterns);
 		expect(result).toEqual([]);
 	});
 
-	it("throws an error when given an invalid pattern", () => {
+	it("throws an error when given a pattern without a template", () => {
 		const patterns = "Folder";
 		expect(() => parsePatterns(patterns)).toThrow(
 			'Invalid format: "Folder". Use "Folder:Template" format.'
 		);
 	});
 
-	it("returns a matcher for a simple text pattern", () => {
+	it("throws an error when given a pattern with too many elements", () => {
+		const patterns = "Folder:Template:Extra";
+		expect(() => parsePatterns(patterns)).toThrow(
+			'Invalid format: "Folder:Template:Extra". Use "Folder:Template" format.'
+		);
+	});
+
+	it("parses a simple text pattern", () => {
 		const patterns = "Folder:Template";
 		const matchers = parsePatterns(patterns);
 		expect(matchers).toHaveLength(1);
@@ -22,7 +29,14 @@ describe("parsePatterns", () => {
 		expect(matchers[0]("OtherFolder")).toBeUndefined();
 	});
 
-	it("returns matchers for multiple simple text patterns", () => {
+	it("parses a simple text pattern w/ spaces", () => {
+		const patterns = "My Folder:My Template";
+		const matchers = parsePatterns(patterns);
+		expect(matchers).toHaveLength(1);
+		expect(matchers[0]("My Folder")).toEqual("My Template");
+	});
+
+	it("parses multiple simple text patterns", () => {
 		const patterns = "Folder:Template\nFolder2:Template2";
 		const matchers = parsePatterns(patterns);
 		expect(matchers).toHaveLength(2);
@@ -30,7 +44,7 @@ describe("parsePatterns", () => {
 		expect(matchers[1]("Folder2")).toEqual("Template2");
 	});
 
-	it("returns a matcher for a regex pattern", () => {
+	it("parses a regex pattern", () => {
 		const patterns = "/Folder\\d/:Template";
 		const matchers = parsePatterns(patterns);
 		expect(matchers).toHaveLength(1);
@@ -40,7 +54,7 @@ describe("parsePatterns", () => {
 		expect(matchers[0]("Folder/Subfolder")).toBeUndefined();
 	});
 
-	it("returns a matcher for a wildcard pattern", () => {
+	it("parses a wildcard pattern", () => {
 		const patterns = "*:Template";
 		const matchers = parsePatterns(patterns);
 		expect(matchers).toHaveLength(1);
@@ -49,7 +63,7 @@ describe("parsePatterns", () => {
 		expect(matchers[0]("Deeply/Nested/Folder")).toEqual("Template");
 	});
 
-	it("returns a matcher for a longer regex pattern", () => {
+	it("parses a longer regex pattern", () => {
 		const patterns = "/^Dates\\/\\d{4}-\\d{2}-\\d{2}$/:Daily";
 		const matchers = parsePatterns(patterns);
 		expect(matchers).toHaveLength(1);
